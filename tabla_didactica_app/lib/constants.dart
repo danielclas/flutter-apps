8bdc2a95715425d5fa44b8880404590d061dc7f1
danielclas <danielclas@outlook.es>
2020-09-21 16:16:58 -0300

    Ejercicios

---
+++ tabla_didactica_app/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const List<String> kLanguages = ['English', 'Spanish', 'Portuguese'];
+const List<List<String>> kColors = [
+  ['Red', 'Blue', 'Yellow', 'Purple', 'Green'],
+  ['Rojo', 'Azul', 'Amarillo', 'Rojo', 'Verde'],
+  ['Vermelho', 'Azul', 'Amarelo', 'Roxo', 'Verde'],
+];
+const List<List<String>> kAnimals = [
+  ['Dog', 'Cat', 'Lion', 'Dolphin', 'Tiger'],
+  ['Perro', 'Gato', 'Leon', 'Deflin', 'Tigre'],
+  ['Cachorro', 'Gato', 'Leão', 'Deflin', 'Tigre']
+];
--- tabla_didactica_app/lib/home_page.dart
+++ tabla_didactica_app/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,27 +9,79 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String selectedLanguage;
+  int selectedLanguage = 0;
+  String selectedSection = "Animales";
+
+  void switchLanguage() {
+    selectedLanguage = selectedLanguage == 2 ? 0 : selectedLanguage + 1;
+  }
 
   List<Widget> makeRows() {
     List<Widget> list = [];
-    list.add(Text(selectedLanguage));
-    list.add(ColoredBox(color: Colors.red));
-    list.add(Text("UNO"));
-    list.add(Text("UNO"));
+
+    list.add(Expanded(
+        child: SizedBox.expand(child: Center(child: Text(selectedSection)))));
+
+    for (int i = 0; i < 4; i++) {
+      String color = kColors[0][i];
+      list.add(Expanded(
+          child: SizedBox.expand(
+              child: DecoratedBox(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Text(kAnimals[0][i]), Text(kColors[0][i])],
+        ),
+        decoration: BoxDecoration(color: Colors.red),
+      ))));
+    }
 
     return list;
   }
 
   @override
   void initState() {
-    selectedLanguage = Languages.English.toString();
+    selectedLanguage = "English";
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: Stack(
+          children: <Widget>[
+            Positioned(
+              bottom: 80.0,
+              right: 10.0,
+              child: FloatingActionButton(
+                heroTag: 'save',
+                onPressed: () {
+                  setState(() {});
+                },
+                child: Icon(Icons.save),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 10.0,
+              right: 10.0,
+              child: FloatingActionButton(
+                heroTag: 'close',
+                onPressed: () {
+                  setState(() {
+                    switchLanguage();
+                  });
+                },
+                child: Icon(Icons.close),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0.0),
+                ),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
             title: Text('Relevamiento visual',
                 style: TextStyle(color: Colors.white, fontFamily: 'Orbitron'))),
@@ -45,5 +98,3 @@ class _HomeState extends State<Home> {
         ));
   }
 }
-
-enum Languages { English, Spanish, Portuguese }
