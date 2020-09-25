f6642a7d573b64db158ece6bfbe913064efe7126
danielclas <danielclas@outlook.es>
2020-09-25 09:15:16 -0300

    message

--- push.bat
+++ push.bat
@@ -3,6 +3,8 @@ cls
 git config user.name 'danielclas'
 git config user.email 'danielclas@outlook.es'
 git add .
-git commit -m "Ejercicios"
+echo "Commit message: "
+read message
+git commit -m message
 git push
 cls
