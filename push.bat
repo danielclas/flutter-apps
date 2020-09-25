656ecf819073dda1eff918eaf0eaf11138b42ea9
danielclas <danielclas@outlook.es>
2020-09-25 09:17:41 -0300

    Change push bash

--- push.bat
+++ push.bat
@@ -3,8 +3,7 @@ cls
 git config user.name 'danielclas'
 git config user.email 'danielclas@outlook.es'
 git add .
-echo "Commit message: "
-read message
-git commit -m message
+set /P message=Enter message: 
+git commit -m %message%
 git push
 cls
